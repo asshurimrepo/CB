45a413c7c3abd22240d39e3f6f4b978ad31131c3
rigz <rigorcadiz@gmail.com>
2016-08-04 22:04:38 +0800

    Finished Actions Modal with laravel Framework and default values

--- resources/assets/js/components/ActionsFBOverlay.js
+++ resources/assets/js/components/ActionsFBOverlay.js
@@ -13,6 +13,30 @@ export default {
         }
     },
 
+    computed: {
+    	formoverlay_titlesize() {
+    		if(this.project.actions.formoverlay_titlesize === 'Small') return '14px';
+    		if(this.project.actions.formoverlay_titlesize === 'Medium') return '18px';
+    		if(this.project.actions.formoverlay_titlesize === 'Large') return '22px';
+
+    		return '18px';
+    	},
+    	formoverlay_fieldsize(){
+    		if(this.project.actions.formoverlay_fieldsize === 'Small') return 'input-sm';
+    		if(this.project.actions.formoverlay_fieldsize === 'Medium') return '';
+    		if(this.project.actions.formoverlay_fieldsize === 'Large') return 'input-lg';
+
+    		return '';
+    	},
+    	formoverlay_buttonsize(){
+    		if(this.project.actions.formoverlay_buttonsize === 'Small') return 'btn-sm';
+    		if(this.project.actions.formoverlay_buttonsize === 'Medium') return '';
+    		if(this.project.actions.formoverlay_buttonsize === 'Large') return 'btn-lg';
+
+    		return 'btn-sm';    		
+    	}
+    },
+
     methods: {
         updateSwitchable($this) {
             this.$set('project.actions.' + $this.target.id, $this.target.checked);
