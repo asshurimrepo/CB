348e9e5c2b66d21aa40f8010604fe859832a25e0
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-08-14 23:27:00 +0800

    add blueimp plugin

---
+++ resources/views/empty.blade.php
@@ -0,0 +1,5 @@
+<section class="error-wrapper" style="margin-top: 6%" v-if="projects.length < 1">
+        <img src="/img/cry.png" alt="Caster Buddy No Videos" style="max-width: 400px; width: 100%; margin: 10px;">
+        <h3>I have no videos. Rendering me worthless.</h3>
+        <h4 class="page-505">Now I am sad. <a href="index.html">Fix Me!</a></h4>
+ </section>
