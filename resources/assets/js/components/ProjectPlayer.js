01aec94e4d7355a1a4f5c6dc6b39e3e6506510fd
rigz <rigorcadiz@gmail.com>
2016-08-17 22:25:31 +0800

    Actions update

--- resources/assets/js/components/ProjectPlayer.js
+++ resources/assets/js/components/ProjectPlayer.js
@@ -25,24 +25,54 @@ export default {
 				position: "",
 				dimmed: false,
 				glass: false
-			}
+			},
+
+			textoverlay_class: {
+				valignment: "",
+				alignment: ""
+			},
+
+			video_duration: 0
 		}
 	},
 
-	watch: {
-		project() {
-			this.updatePlayer();
-			this.projectOptions();
-			this.projectActions();
-		}
-	},
 
 	events: {
 		show_preview() {
 			setTimeout(() => {
 				this.updatePlayer();
 				this.playProject();
+				this.projectOptions();
+				this.projectActions();
 			}, 200);
+		}
+	},
+
+	computed: {
+		has_Textoverlay(){
+			let line1 = this.project.actions.textoverlay_line_1;
+			let line2 = this.project.actions.textoverlay_line_2;
+
+			if( line1 === "" && line2 === ""){
+				return false;
+			}
+			return true;
+		},
+
+		has_Line1(){
+			let line1 = this.project.actions.textoverlay_line_1;
+			if( line1 == ""){
+				return false
+			}
+			return true;
+		},
+
+		has_Line2(){
+			let line2 = this.project.actions.textoverlay_line_2;
+			if( line2 == ""){
+				return false
+			}
+			return true;
 		}
 	},
 
@@ -98,36 +128,41 @@ export default {
 
 		playProject() {
 			let delay = parseInt(this.project.options.auto_display_after)*1000;
+			this.video_duration = 0;
 
 			setTimeout(() => {
 				this.is_visible = true;
-				$("#project-player-bg").show();
+				$('#project-player-bg').fadeIn("fast");
 				this.video.play();
+				this.video_duration = Math.floor(this.video.duration())*1000;
 			}, delay);
 
 			// close button function
-			$("body").on("click","a.close-project", () => {
+			$("body").on("click","a.close-project", (e) => {
+				e.preventDefault();
 				this.video.pause();
-				$('#project-player-bg').hide();
+				$('#project-player-bg').fadeOut("fast");
 			});
 
 		},
 
 		projectOptions(){
-			let exit =  this.project.options.stop_showing.exit_on_end;
-			let closed_click = this.project.options.stop_showing.clicked;
-
 			// if close on exit is true
 			this.video.on("ended", () =>{
-				if(exit){
-					$('#project-player-bg').hide();
+				if(this.project.options.stop_showing.exit_on_end === true){
+					$('#project-player-bg').fadeOut("fast");
 				}
 			});
 
 			// if close on click is true
-			if(closed_click){
-				$("#project-player-container").on("click",() => {
-					$("#project-player-bg").hide();
+			if(this.project.options.stop_showing.clicked === true){
+				$("#project-player-container").on("click",(e) => {
+					if($(e.target).is('video#project-player_html5_api')){
+						this.video.pause();
+						$('#project-player-bg').fadeOut("fast");
+					}
+					e.preventDefault();
+			        return;
 				});
 			}
 
@@ -139,9 +174,41 @@ export default {
 
 			//link url
 			if(url_length > 0){
-				$("#project-player-container").wrap("<a href='"+ url +"' target='_blank'></a>");
+				$("#project-player-container").one("click",(e) => {
+					if($(e.target).is('video#project-player_html5_api')){
+						window.open (url, '_blank');
+			        }
+			        e.preventDefault();
+			        return;
+				});
 			}
-		}
+
+			//textoverlay valignment
+			if(this.project.actions.textoverlay_valignment == 'middle'){
+				this.textoverlay_class.valignment = "Textoverlay--middle";
+			}
+
+			if (this.project.actions.textoverlay_valignment == 'top'){
+				this.textoverlay_class.valignment = "Textoverlay--top";
+			}
+
+			if(this.project.actions.textoverlay_valignment == 'bottom'){
+				this.textoverlay_class.valignment = "Textoverlay--bottom";
+			}
+
+			//textoverlay alignment
+			if(this.project.actions.textoverlay_alignment == 'left'){
+				this.textoverlay_class.alignment = "Textoverlay--left";
+			}
+
+			if (this.project.actions.textoverlay_alignment == 'center'){
+				this.textoverlay_class.alignment = "Textoverlay--center";
+			}
+
+			if(this.project.actions.textoverlay_alignment == 'right'){
+				this.textoverlay_class.alignment = "Textoverlay--right";
+			}
+		} //end of projectActions
 
 	}
 }
