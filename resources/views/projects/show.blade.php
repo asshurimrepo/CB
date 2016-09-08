0a333cc769fa46642279b98fe26d4eb596271aba
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-09-08 17:45:09 +0800

    Todd Gross

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,124 +1,117 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Seriously Testing</title>
+</head>
 
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>  
+    <script src="http://hello.p5js.org/js/vendor/seriously.js"></script>
+    <script src="http://hello.p5js.org/js/vendor/effects/seriously.chroma.js"></script>
+    <script>
+      $(document).ready(function() {
 
-    <video preload="auto" id="video" width="400" height="450">
-         <source src="/video/FDntFDhAIWdogTraining.mp4" type="video/mp4">
-    </video>
+        $("#video").bind("loadeddata", function(){
 
-	<button onclick="playVideo();">Play</button>
-	<button onclick="startProcessing();">Old Script</button>
-	<button onclick="startProcessing2();">New Script</button>
-	<button onclick="stopProcessing();">Stop</button>
+          var seriously,
+            chroma,
+            target;
 
+          seriously = new Seriously();
 
-    <canvas  width="400" height="450" id="buffer"></canvas>
-	<canvas width="400" height="450" id="output" style="border: 1px solid #000;"></canvas>
-	<canvas width="400" height="450" id="output2" style="border: 1px solid #000;"></canvas>
+          target = seriously.target('#canvas');
+          chroma = seriously.effect('chroma');
 
+          chroma.source = "#video";
+          target.source = chroma;
+          chroma.screen = [0,1,0,1];
+          chroma['clipBlack'] = 1;
+          chroma['weight'] = 2;
+          seriously.go();
 
-<script type="text/javascript">
-	// OhE1P7udxRZaHftw2
-	// MjsoyK9dxFTHREE
-	var vPlayer = document.getElementById("video");
-	var buffer = document.getElementById("buffer").getContext('2d');
-	var output = document.getElementById("output").getContext('2d');
-	var output2 = document.getElementById("output2").getContext('2d');
-	var w = 400;
-	var h = 450/2;
-	var dy = 0;
- 	var frstfrm = true;
+          $("input[type=range]").bind("input",function(e) { 
+            update(this);
+          });
 
-	function _processFrame() {
-		buffer.drawImage(vPlayer, 0, 0);
+          function update(elment) {
+            var id = $(elment).attr('id')
+            var value = $(elment).val();
 
-		//var	image=buffer.getImageData(0, 0, w, h-dy-7);
-		//var alpha=buffer.getImageData(0, h-dy, w, h-dy-7);
+            $("#"+id+"Value").html(value);
 
-		var	image=buffer.getImageData(0, 0, w, h-dy-0);
-		var alpha=buffer.getImageData(0, h-dy, w, h-dy-0);
-		var	imageData=image.data;
-		var	alphaData=alpha.data;
-		if(frstfrm) {
-			if(navigator.userAgent.toLowerCase().indexOf('firefox')<0) {
-				if(alphaData[w*4]<50) dy=Math.round(6-(400-w)/50)+1;
-				console.log(dy);
-			}
-	      frstfrm=false;
-		} else {
-			var strt=w*0+3;
-			var len=imageData.length;
-			for(var i=strt; i<len; i+=4) imageData[i]=alphaData[i-1];
-	//		output.putImageData(image, 0, 0, 0, 0, w, h-6);
-			output.putImageData(image, 0, 0, 0, dy, w, h-dy);
-	    }
-	}
+            if ($.inArray(id, ['red','green','blue']) > - 1) {
+              var red = parseFloat($("#red").val());
+              var green = parseFloat($("#green").val());
+              var blue = parseFloat($("#blue").val());
+              id = "screen";
+              value = [red,green,blue,1];
+              chroma.screen = value;
+            }
 
-	function _processFrame2() {
-		buffer.drawImage(vPlayer, 0, 0);
+            chroma[id] = value;
+          }
 
-		var	image=buffer.getImageData(0, 0, w, h);
+        });
 
+      });
+    </script> 
 
-		// Step 3: Go pixel by pixel and detect whether or not we're at the green screen.
-		for ( var i=0; i<image.data.length; i+=4 ) {
-			var makeTrans = false;
+ <style>
+      body{
+        margin:0;
+        padding:0;
+        background: #eee;
+      }
+      #video {
+        position:absolute;
+        width: 320px;
+        height: 240px;
+        top: 0;
+        left: 0;
+        z-index: 2;
+      }
+      #background {
+        background: rgb(255,0,255); 
+        position:absolute;
+        width: 480px;
+        height: 720px;
+        top: 0;
+        left: 0;              
+      }
+      #canvas {
+        position:absolute;
+        bottom: 0;
+        right: 0;  
+        z-index:1;  
+        width: 400px;          
+      }      
+      #controls {
+        position:absolute;
+        top:240px;
+        z-index:3;
+      }
+    </style> 
+<body>
 
-			// if green is super high, filter it out, since it's green screen.
-			if ( image.data[i+1] > 230 ) {
-				makeTrans = true;
-			}
-			// if green is high but not super high, check other colors.
-			else if ( image.data[i+1] > 100 ) {
-				// if the ratio of the average of green to the other colors is > 1.7, it's green screen.
-				if ( image.data[i+1]/((image.data[i]+image.data[i+2])/2) > 1.7 ) {
-					makeTrans = true;
-				}
-				// left side filter, since there is color blending at the edges of Ty.
-				if ( image.data[i+1] > 130 && image.data[i-1]==0 ) {
-					// makeTrans = true;
-				}
-				// // right side filter, for the same reason.
-				if ( image.data[i+1] > 130 && (image.data[i+5]/((image.data[i+4]+image.data[i+6])/2) > 1.6 || image.data[i+5] > 230) ) {
-					// makeTrans = true;
-				}
-			}
-			// Otherwise, reduce the overall green of that pixel. The green bounced back onto his face
-			// during filming, the colors blended, etc. Basically reduce the green effect.
-			else {
-				// image.data[i+1] *= 0.7;
-			}
-
-			// Make transparent the green screen pixel.
-			if ( makeTrans ) { image.data[i+3] = 0; }
-		}
-
-		// console.log(imageData);
-		output2.putImageData(image, 0, 0);
-
-	}
-
-	function startProcessing() {
-		timer = setInterval(_processFrame, 100);
-	}
-
-	function startProcessing2() {
-		timer = setInterval(_processFrame2, 10);
-	}
-
-	function stopProcessing() {
-		clearInterval(timer);
-		parent.postMessage("close", "*");
-	}
-
-	function startPlaying() {
-		outputCanvas.setAttribute("width", w);
-		outputCanvas.setAttribute("height", h);
-
-		setTimeout(function(){ vPlayer.play(); }, 3000);
-	}
-
-	function playVideo() {
-		vPlayer.play();
-	}
-
-
-</script>
+ 
+<video id="video"  controls preload>
+<source src="/todd.mp4" type="video/mp4" />  
+</video>      
+<canvas id="canvas" width="1280" height="720"></canvas> 
+<div id="controls">
+        <label>weight: <span id="weightValue">1</span></label><br/>
+        <input type="range" id="weight" min="0" max="2" step="0.01" value="2"/><br/>    
+        <label>clipWhite: <span id="clipWhiteValue">1</span></label><br/>
+        <input type="range" id="clipWhite" min="0" max="1" step="0.01" value="1"/><br/> 
+        <label>clipBlack: <span id="clipBlackValue">0</span></label><br/>
+        <input type="range" id="clipBlack" min="0" max="1" step="0.01" value="1"/><br/> 
+        <label>balance: <span id="balanceValue">1</span></label><br/>
+        <input type="range" id="balance" min="0" max="1" step="0.01" value="1"/><br/>       
+        <label>red: <span id="redValue"></span>0</label><br/>
+        <input type="range" id="red" min="0" max="1" step="0.01" value="0"/><br/>  
+        <label>green: <span id="greenValue">1</span></label><br/>
+        <input type="range" id="green" min="0" max="1" step="0.01" value="1"/><br/>
+        <label>blue: <span id="blueValue">0</span></label><br/>
+        <input type="range" id="blue" min="0" max="1" step="0.01" value="0"/><br/>  
+      </div>
+</body>
+</html>
