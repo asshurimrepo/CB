57377fe0ff056ec93f8a5091ee60337026b02cd0
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-09-21 18:28:27 +0800

    480p conversion

--- app/Http/Controllers/UploadProjectsController.php
+++ app/Http/Controllers/UploadProjectsController.php
@@ -48,9 +48,9 @@ class UploadProjectsController extends Controller
         $file = $request->file('file')->move("data/{$user->email}/videos", $filename);
 
         $commands = [
-            "ffmpeg -i {$file->getPathname()} -vcodec libx264 -preset veryfast -threads 2 -acodec aac -b:a 128k -f mp4 data/{$user->email}/done/{$file->getFilename()}",
+            "ffmpeg -i {$file->getPathname()} -vcodec libx264 -preset veryfast -vf scale=720:480 -threads 2 -acodec aac -b:a 128k -f mp4 data/{$user->email}/done/{$file->getFilename()}",
 
-        "ffmpeg -i {$file->getPathname()} -ss 00:00:01 -vframes 1 -vf scale=-1:-1 data/{$user->email}/images/{$filename}.png",
+            "ffmpeg -i {$file->getPathname()} -ss 00:00:01 -vframes 1 -vf scale=-1:-1 data/{$user->email}/images/{$filename}.png",
 
             "cd data/{$user->email}/images/ && gimp --verbose -i -d -f -b '(create-thumb \"{$filename}.png\")' -b '(gimp-quit 0)' && \
              cp ../out/thumb.png ../done/{$filename}.png && \
