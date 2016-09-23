ef37d8ffa8d079673c090b61ed484d572b298966
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-09-23 17:00:24 +0800

    allow cross domain

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
 Route::auth();
 
 Route::group(['middleware' => 'web'], function(){
+
+	header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
 	// Handles Showing Image
 	Route::get('/image/{filename}', 'ImagesController@show');
 	Route::get('/embed/image/{user}/{filename}', 'ImagesController@embed');
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,16 +3,16 @@
   <meta charset="UTF-8">
   <title>Embed Tests</title>
 </head>
-<body><link rel="stylesheet" href="http://caster-buddy.dev/css/project-player.css" />
+<body><link rel="stylesheet" href="http://casterbuddy.com/css/project-player.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
-<script src="http://caster-buddy.dev/js/seriously.js" type="text/javascript"></script>
-<script src="http://caster-buddy.dev/js/seriously.chroma.js" type="text/javascript"></script>
+<script src="http://casterbuddy.com/js/seriously.js" type="text/javascript"></script>
+<script src="http://casterbuddy.com/js/seriously.chroma.js" type="text/javascript"></script>
 <link href="http://vjs.zencdn.net/5.10.8/video-js.css" rel="stylesheet">
 <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 <script>
         var casterScript = document.createElement('script');
-        casterScript.setAttribute("src", "http://caster-buddy.dev/embed/caster.js?ID=10");
+        casterScript.setAttribute("src", "http://casterbuddy.com/embed/caster.js?ID=279");
         document.querySelector('body').appendChild(casterScript);
 </script>
-<div id="caster-embed-10"><project-player :project="active_project"></project-player></div></body>
+<div id="caster-embed-279"><project-player :project="active_project"></project-player></div></body>
 </html>
