3f36207d232d5753ef46b7e12616f468cc816040
OHL-01 <OHL-01>
2016-09-27 15:45:57 +0800

    Testing

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,3 +1,9 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Accept-Language, Content-Language, Authorization, X-Request-With, X-Access-Token, X-Application-Name, X-Request-Sent-Time');
+ ?>
 <html>
 <head>
   <meta charset="UTF-8">
