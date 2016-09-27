de030f32642fbb9372f75236242c4b8a8bc7ea86
OHL-01 <OHL-01>
2016-09-27 19:03:14 +0800

    Updated show.blade.php

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,16 +3,19 @@
   <meta charset="UTF-8">
   <title>Embed Tests</title>
 </head>
-<body><link rel="stylesheet" href="http://casterbuddy.dev/css/project-player.css" />
+<body>
+<link rel="stylesheet" href="http://casterbuddy.com/css/project-player.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
-<script src="http://casterbuddy.dev/js/seriously.js" type="text/javascript"></script>
-<script src="http://casterbuddy.dev/js/seriously.chroma.js" type="text/javascript"></script>
+<script src="http://casterbuddy.com/js/seriously.js" type="text/javascript"></script>
+<script src="http://casterbuddy.com/js/seriously.chroma.js" type="text/javascript"></script>
 <link href="http://vjs.zencdn.net/5.10.8/video-js.css" rel="stylesheet">
 <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 <script>
         var casterScript = document.createElement('script');
-        casterScript.setAttribute("src", "http://casterbuddy.dev/embed/caster.js?ID=9");
+        casterScript.setAttribute("src", "http://casterbuddy.com/embed/caster.js?ID=270");
         document.querySelector('body').appendChild(casterScript);
 </script>
-<div id="caster-embed-9"><project-player :project="active_project"></project-player></div></body>
+<div id="caster-embed-270"><project-player :project="active_project"></project-player></div>
+
+</body>
 </html>
