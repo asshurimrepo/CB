2fd851026c881e882f5db079427cd0744afefe77
OHL-01 <OHL-01>
2016-10-04 15:43:12 +0800

    New testing

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,7 +4,8 @@
   <title>Embed Tests</title>
 </head>
 <body>
-<link rel="stylesheet" href="http://casterbuddy.com/css/project-player.css" /> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" /> <script src="http://casterbuddy.com/js/seriously.js" type="text/javascript"></script> <script src="http://casterbuddy.com/js/seriously.chroma.js" type="text/javascript"></script> <link href="http://vjs.zencdn.net/5.10.8/video-js.css" rel="stylesheet"> <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script> <script> var casterScript = document.createElement('script'); casterScript.setAttribute("src", "http://casterbuddy.com/embed/caster.js?ID=282"); document.querySelector('body').appendChild(casterScript); </script> <div id="caster-embed-282"><project-player :project="active_project"></project-player></div> 
+<link rel="stylesheet" href="http://casterbuddy.com/css/project-player.css" /> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" /> <script src="http://casterbuddy.com/js/seriously.js" type="text/javascript"></script> <script src="http://casterbuddy.com/js/seriously.chroma.js" type="text/javascript"></script> <link href="http://vjs.zencdn.net/5.10.8/video-js.css" rel="stylesheet"> <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script> <script> var casterScript = document.createElement('script'); casterScript.setAttribute("src", "http://casterbuddy.com/embed/caster.js?ID=281"); document.querySelector('body').appendChild(casterScript); </script> <div id="caster-embed-281"><project-player :project="active_project"></project-player></div> 
+                
                 
 </body>
 </html>
