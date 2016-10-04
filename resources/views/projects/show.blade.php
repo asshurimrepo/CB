c7783123489e8c93854e63480da80af6a1fd47c1
OHL-01 <OHL-01>
2016-10-04 21:37:49 +0800

    Fixed css issues

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,5 +3,5 @@
   <meta charset="UTF-8">
   <title>Embed Tests</title>
 </head>
-<body><script src="http://casterbuddy.dev/embed.js/12"></script></body>
+<body><script src="http://casterbuddy.com/embed.js/281"></script></body>
 </html>
