d0993a7b49fd44e1a10c7f142edbaeffae12dc9e
OHL-01 <OHL-01>
2016-10-04 17:02:42 +0800

    Test-2

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,5 +3,5 @@
   <meta charset="UTF-8">
   <title>Embed Tests</title>
 </head>
-<body><script src="http://casterbuddy.com/embed.js/283"></script></body>
+<body><script src="http://casterbuddy.dev/embed.js/12"></script></body>
 </html>
