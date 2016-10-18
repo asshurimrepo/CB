fafeca7ed9341cc7acf17f1f97e58a8465b7420e
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-10-18 22:17:24 +0800

    fix embed

--- public/js/embed.js
+++ public/js/embed.js
@@ -12,7 +12,16 @@ fontAwesomeStyle.setAttribute('rel', 'stylesheet');
 fontAwesomeStyle.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css");
 
 
+/*Elements*/
+var container_div = document.createElement("div");
+container_div.setAttribute('id', 'caster-embed-@id');
+var component_player = document.createElement("project-player");
+component_player.setAttribute(':project', 'active_project');
+container_div.appendChild(component_player);
+
 /*Add to DOM*/
 document.querySelector('head').appendChild(playerStyle);
 document.querySelector('head').appendChild(fontAwesomeStyle);
+
+document.querySelector('body').appendChild(container_div);
 document.querySelector('body').appendChild(casterJS);
