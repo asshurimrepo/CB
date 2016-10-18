227374d98c89d0bc1b60338b61a8d3d4f7f65a8b
rigz <rigorcadiz@gmail.com>
2016-10-18 14:05:08 +0800

    Users unfinished

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,6 +6,7 @@
 <body>
 <!-- <script src="http://casterbuddy.com/embed.js/281"></script> -->
 <!-- <script src="http://casterbuddy.dev/embed.js/13"></script> -->
-<script src="http://casterbuddy.com/embed.js/281"></script>
+<!-- <script src="http://casterbuddy.com/embed.js/281"></script> -->
+<script src="http://casterbuddy.com/embed.js/282"></script>
 </body>
 </html>
