83baa03a1617e62facc0288abd472002bd61eb11
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-10-18 16:35:59 +0800

    include test on iframe

---
+++ resources/views/embed/iframe_test.blade.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Caster Buddy Test Embed</title>
+</head>
+<body>
+	
+	<iframe style="position: absolute; right: 0; bottom: 0;" src="http://caster-buddy.dev/embed/iframe/19" frameborder="0" width="300px" scrolling="no"></iframe>
+    
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	
+	<script>
+		var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+		var eventer = window[eventMethod];
+		var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+		// Listen to message from child window
+		eventer(messageEvent,function(e) {
+		    var key = e.message ? "message" : "data";
+		    var data = e[key];
+
+		    $("iframe").height(data.height);
+
+
+		    console.log(data);
+		},false);
+	</script>
+
+</body>
+</html>
