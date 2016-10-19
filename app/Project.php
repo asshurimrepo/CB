4a0e7f034607d1d597fab8fd779b5940ab2880e6
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-10-19 20:18:47 +0800

    add API for isnew and isupdated

--- app/Project.php
+++ app/Project.php
@@ -3,11 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Project extends Model
 {
 	use Projects\Attributes;
 
     protected $fillable = ['title', 'user_id', 'filename', 'options', 'actions'];
+
+    protected $appends = ['isNew', 'isUpdated'];
+
+
+    public function getIsNewAttribute()
+    {
+    	$days = $this->created_at->diff(Carbon::now())->days;
+    	return $days < 1 ? true : false;
+    }
+
+    public function getIsUpdatedAttribute()
+    {
+    	$days = $this->updated_at->diff(Carbon::now())->days;
+    	return $days < 1 ? true : false;
+    }
 
 }
