08b5c5165b7afaa803ef271470a4cb8fe5de0feb
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-10-21 13:24:35 +0800

    Generate webm

--- public/premades/generatewebm.sh
+++ public/premades/generatewebm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for filename in *.mp4; do
-    ffmpeg -i "$filename" -vcodec libvpx -preset veryfast -vf scale=1000:720 -threads 2  -acodec libvorbis "$filename.webm"
+    ffmpeg -i "$filename" -b:v 0.85M -q:v 10 -vcodec libvpx -vf scale=640:720 -acodec libvorbis "$filename.webm"
 done
