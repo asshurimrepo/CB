50658700ed389bf76a3dffadc74e77ffa686d27d
Asshurim Joy O. Larita <asshurimlarita@gmail.com>
2016-10-21 14:22:29 +0800

    Add generate webm

--- public/premades/generatewebm.sh
+++ public/premades/generatewebm.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 for filename in *.mov; do
-    ffmpeg -i "$filename" -b:v 0.85M -q:v 10 -vcodec libvpx -vf scale=640:720 -acodec libvorbis "$filename.webm"
+    ffmpeg -i "$filename" -b:v 0.85M -q:v 10 -vcodec libvpx -vf scale=996:560 -acodec libvorbis "$filename.webm"
+done
+
+for filename in *.mp4; do
+    ffmpeg -i "$filename" -b:v 0.85M -q:v 10 -vcodec libvpx -vf scale=996:560 -acodec libvorbis "$filename.webm"
 done
